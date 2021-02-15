c842c27ec4508dc6c18045f7b9192eb8dfbd81c3
PleasantJ <ys.hazard@gmail.com>
2021-02-15 15:55:06 +0900

    init commit

---
+++ AboutViewModel/app/src/main/java/pleasand/j/aboutviewmodel/MainActivity.kt
@@ -0,0 +1,18 @@
+package pleasand.j.aboutviewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pleasand.j.aboutviewmodel.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ AboutViewModel/app/src/main/java/pleasand/j/aboutviewmodel/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package pleasand.j.aboutviewmodel.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
