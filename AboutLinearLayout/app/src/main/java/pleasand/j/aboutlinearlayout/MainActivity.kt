4b0ac9e9bdc9d790264c4fa4172bb3e178ef0f7b
PleasantJ <ys.hazard@gmail.com>
2021-02-16 14:55:31 +0900

    LinearLayout 으로 화면 구성 설명추가

--- AboutLinearLayout/app/src/main/java/pleasand/j/aboutlinearlayout/MainActivity.kt
+++ AboutLinearLayout/app/src/main/java/pleasand/j/aboutlinearlayout/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+         * 선형 레이아웃, LinearLayout
+         * 세로 또는 가로 방향으로 모든 하위 요소를 정렬하는 그룹.
+         * 가중치(Weight) 가 존재, 높을 수록 더 많은 영역을 차지.
+         */
     }
 }
