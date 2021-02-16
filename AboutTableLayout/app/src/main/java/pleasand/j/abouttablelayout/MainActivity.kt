c890faa583a3043a48bb8a1d8dd1649c5fbb3d1d
PleasantJ <ys.hazard@gmail.com>
2021-02-16 15:28:13 +0900

    TableLayout 으로 화면구성 설명 추가

--- AboutTableLayout/app/src/main/java/pleasand/j/abouttablelayout/MainActivity.kt
+++ AboutTableLayout/app/src/main/java/pleasand/j/abouttablelayout/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * 표, TableLayout.
+         * 격자로 뷰를 정렬, 배치할 수 있으며,
+         * 하위 뷰는 layout_span 또는 layout_colum 으로 위치를 결정할 수 있음.
+         * TableLayout 의 stretchColumns 은 하위 뷰의 총 개수를 지정가능.
+         */
     }
 }
